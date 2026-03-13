669ddad18843a5699d4ab90907a31287a8ceba35
Olexandr <sanny0787@gmail.com>
2026-03-13 07:36:06 +0200

    feat(Lesson 5): task 2

---
+++ CSharp_Essential/Lesson_5_Arrays_Indexers/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Random random = new Random();
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+    array[i] = random.Next(0, 100);
+
+for (int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + " ");
+int max = array[0];
+int min = array[0];
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        max = array[i];
+    if (array[i] < min)
+        min = array[i];
+    sum += array[i];
+    if (array[i] % 2 != 0)
+        Console.WriteLine($"\n{array[i]} - непарне число");
+}
+Console.WriteLine($"\nМаксимальное число: {max}");
+Console.WriteLine($"Минимальное число: {min}");
+Console.WriteLine($"Середнє арифметичне чисел: {sum/array.Length}");
