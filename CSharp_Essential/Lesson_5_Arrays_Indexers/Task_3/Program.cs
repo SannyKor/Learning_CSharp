8d8aad406bad873ec64c3c4031487c247814d292
Olexandr <sanny0787@gmail.com>
2026-03-14 07:28:06 +0200

    feat(Lesson 5): task 3

---
+++ CSharp_Essential/Lesson_5_Arrays_Indexers/Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using Task_3;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+bool exit = false;
+while (!exit)
+    {
+    Console.WriteLine("Введіть кількість рядків матриці:");
+    int rows = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введіть кількість стовпців матриці:");
+    int cols = int.Parse(Console.ReadLine());
+    MyMatrix matrix = new MyMatrix(rows, cols);
+
+    Console.WriteLine("\nЗгенерована матриця та її похідні:");
+    matrix.PrintMatrix();
+    matrix.PrintDerivedMatrixs();
+
+    Console.WriteLine("\nБажаєте створити іншу матрицю? (y/n)");
+    string choice = Console.ReadLine().ToLower();
+    if (choice != "y")
+        exit = true;
+}
