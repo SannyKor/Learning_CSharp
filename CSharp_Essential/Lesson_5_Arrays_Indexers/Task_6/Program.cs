fe5b4e35c6f510496a209e931b1a024d587c385a
Olexandr <sanny0787@gmail.com>
2026-03-15 08:21:14 +0200

    feat(Lesson 5): task 6

---
+++ CSharp_Essential/Lesson_5_Arrays_Indexers/Task_6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+// Індексатори.
+
+namespace Indexers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Dictionary dictionary = new Dictionary();
+
+            Console.WriteLine("Пошук за українською мовою:");
+            Console.WriteLine(dictionary["книга"]);
+            Console.WriteLine(dictionary["дім"]);
+            Console.WriteLine(dictionary["ручка"]);
+            Console.WriteLine(dictionary["стіл"]);
+            Console.WriteLine(dictionary["олівець"]);
+            Console.WriteLine(dictionary["яблуко"]);
+            Console.WriteLine(dictionary["сонце"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine("Пошук за англійською мовою:");
+            Console.WriteLine(dictionary["book"]);
+            Console.WriteLine(dictionary["home"]);
+            Console.WriteLine(dictionary["pen"]);
+            Console.WriteLine(dictionary["table"]);
+            Console.WriteLine(dictionary["pensil"]);
+            Console.WriteLine(dictionary["apple"]);
+            Console.WriteLine(dictionary["sun"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine("Пошук за російською мовою:");
+            Console.WriteLine(dictionary["книга"]);
+            Console.WriteLine(dictionary["дом"]);
+            Console.WriteLine(dictionary["ручка"]);
+            Console.WriteLine(dictionary["стол"]);
+            Console.WriteLine(dictionary["карандаш"]);
+            Console.WriteLine(dictionary["яблуко"]);
+            Console.WriteLine(dictionary["солнце"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
