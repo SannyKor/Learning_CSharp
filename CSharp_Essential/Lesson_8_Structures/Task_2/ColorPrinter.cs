1c82edec18cbe431a48e585af3add5379e567bf4
Olexandr <sanny0787@gmail.com>
2026-03-23 08:14:09 +0200

    feat(Lesson 8): Task_2 in progress

---
+++ CSharp_Essential/Lesson_8_Structures/Task_2/ColorPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    public enum MyColor 
+    { 
+        red = ConsoleColor.Red, 
+        green = ConsoleColor.Green, 
+        blue = ConsoleColor.Blue, 
+        yellow = ConsoleColor.Yellow, 
+        cyan = ConsoleColor.Cyan, 
+        darkblue = ConsoleColor.DarkBlue,
+    }
+    public struct ColorPrinter
+    {
+        public void PrintStr(string str, MyColor color)
+        {
+            ConsoleColor consoleColor = (ConsoleColor)color;
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(str);
+            Console.ResetColor();
+
+        }
+    }
+}
---
+++ CSharp_Essential/Lesson_8_Structures/Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Globalization;
+using Task_2;
+
+ColorPrinter colorPrinter = new ColorPrinter();
+Console.WriteLine("Введіть текст для відображення:");
+string str;
+while(true)
+{
+    str = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(str))
+        break;
+    Console.WriteLine("Щось пішло не так, спробуйте ще.");
+}
+
+MyColor color;
+Console.WriteLine("Виберіть один з кольорів:");
+foreach (var value in Enum.GetValues(typeof(MyColor)))
+{
+    Console.WriteLine($"{(int)value} - {value}");
+}
+while (true)
+{
+    string chosen = Console.ReadLine();
+    if (Enum.TryParse(chosen, true, out color) && Enum.IsDefined(typeof(MyColor), color))
+        break;
+    Console.WriteLine("Щось пішло не так, спробуйте ще.");
+}
+colorPrinter.PrintStr(str, color);
