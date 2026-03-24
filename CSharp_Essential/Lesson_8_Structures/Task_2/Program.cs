64508fbbd03262f56c4ab21da78ae1ff2346e2bc
Olexandr <sanny0787@gmail.com>
2026-03-24 07:35:24 +0200

    fix(Lesson 8): fix incorrect behavior in Task_2

--- CSharp_Essential/Lesson_8_Structures/Task_2/Program.cs
+++ CSharp_Essential/Lesson_8_Structures/Task_2/Program.cs
@@ -2,28 +2,44 @@
 using System.Globalization;
 using Task_2;
 
-ColorPrinter colorPrinter = new ColorPrinter();
-Console.WriteLine("Введіть текст для відображення:");
-string str;
-while(true)
-{
-    str = Console.ReadLine();
-    if (!string.IsNullOrWhiteSpace(str))
-        break;
-    Console.WriteLine("Щось пішло не так, спробуйте ще.");
-}
-
-MyColor color;
-Console.WriteLine("Виберіть один з кольорів:");
-foreach (var value in Enum.GetValues(typeof(MyColor)))
-{
-    Console.WriteLine($"{(int)value} - {value}");
-}
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 while (true)
 {
-    string chosen = Console.ReadLine();
-    if (Enum.TryParse(chosen, true, out color) && Enum.IsDefined(typeof(MyColor), color))
+    ColorPrinter colorPrinter = new ColorPrinter();
+    Console.WriteLine("Введіть текст для відображення:");
+    string str;
+    while(true)
+    {
+        str = Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(str))
+            break;
+        Console.WriteLine("Щось пішло не так, спробуйте ще.");
+    }
+
+    MyColor color;
+    Console.WriteLine("Виберіть один з кольорів:");
+    var colors = (MyColor[])Enum.GetValues(typeof(MyColor));
+    for (int i = 0; i < colors.Length; i++)
+    {
+        Console.WriteLine($"{(i + 1)} - {colors[i]}");
+    }
+
+    while (true)
+    {
+        string chosen = Console.ReadLine();
+        if (Int32.TryParse(chosen, out int colorNumber)&&colorNumber <= colors.Length)
+        { 
+            color = colors[colorNumber - 1];
+            break;
+        }
+        else if (Enum.TryParse(chosen, true, out color) && Enum.IsDefined(typeof(MyColor), color) && colorNumber <= colors.Length)
+            break;
+        Console.WriteLine("Щось пішло не так, спробуйте ще.");
+    }
+        colorPrinter.PrintStr(str, color);
+    Console.WriteLine("Щоб продовжити або закінчити введіть Y/N:");
+    char exit = Console.ReadKey().KeyChar;
+    if (char.ToLower(exit) == 'n')
         break;
-    Console.WriteLine("Щось пішло не так, спробуйте ще.");
+    Console.Clear();
 }
-colorPrinter.PrintStr(str, color);
