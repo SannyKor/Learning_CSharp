907e4f56d729c9b2a84e252c3969f39c427b7f33
Olexandr <sanny0787@gmail.com>
2026-03-29 08:13:07 +0300

    feat(Lesson 9): Implement Task_2

---
+++ CSharp_Essential/Lesson_9_Delegates/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Runtime.CompilerServices;
+
+while (true)
+{
+    Console.Write("Введіть перше число: ");
+    double num1 = double.Parse(Console.ReadLine());
+
+    Console.Write("Введіть друге число: ");
+    double num2 = double.Parse(Console.ReadLine());
+
+    Console.Write("Оберіть операцію (+, -, *, /): ");
+    try
+    {
+        calculationDelegate op = Console.ReadLine() switch
+        {
+            "+" => (a, b) => a + b,
+            "-" => (a, b) => a - b,
+            "*" => (a, b) => a * b,
+            "/" => (a, b) => b != 0 ? a / b : throw new DivideByZeroException("Ділити на нуль не можна!"),
+            "_" => throw new InvalidOperationException("Невідома операція!")
+        };
+        Console.WriteLine($"Результат: {op.Invoke(num1, num2)}"); 
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (InvalidOperationException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    Console.WriteLine("Шоб продовжити обрахунок натисніть 'Y' або 'N' щоб вийти.");
+    char exit = Console.ReadKey(true).KeyChar;
+    if (Char.ToLower(exit) == 'n')
+        break;
+    Console.WriteLine();
+}
+    public delegate double calculationDelegate(double a, double b);
