cdc1702e84b2eaffe1694f5a001cd2b71f5ce029
Olexandr <sanny0787@gmail.com>
2026-04-03 08:30:58 +0300

    feat(Lesson_11): Implement Task_2

---
+++ CSharp_Essential/Lesson_011_Generics(Constraints)/Task_2/CarsCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    public class CarsCollection
+    {
+        private List<Car> cars;
+        
+        public int Count => cars.Count;
+        public CarsCollection()
+        {
+            cars = new List<Car>();
+        }
+        public void AddCar(Car car)
+        {
+            cars.Add(car);
+        }
+        public void RemoveCollection()
+        {
+            Console.WriteLine("Триває видалення...");
+            cars.Clear();
+        }
+        public Car this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= cars.Count)
+                    throw new IndexOutOfRangeException("За цим індексом не закріплена жодна машина");
+                return cars[index];
+            }
+        }
+    }
+}
---
+++ CSharp_Essential/Lesson_011_Generics(Constraints)/Task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using Task_2;
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+CarsCollection carsPark = new CarsCollection();
+
+carsPark.AddCar(new Car(2020, "Tesla Model S"));
+carsPark.AddCar(new Car(2018, "BMW X5"));
+carsPark.AddCar(new Car(2019, "Audi A4"));
+carsPark.AddCar(new Car(2021, "Mercedes-Benz C-Class"));
+carsPark.AddCar(new Car(2017, "Toyota Camry"));
+carsPark.AddCar(new Car(2022, "Porsche 911"));
+
+for (int i = 0; i < carsPark.Count; i++)
+    Console.WriteLine($"Машина {i + 1}: {carsPark[i]}");
+Console.WriteLine("Загальна кількість машин у парку: " + carsPark.Count);
+Console.WriteLine($"Машина за індексом 4: \n\tрік:\t{carsPark[4-1].Year}; \n\tмодель: {carsPark[4-1].Model}");
+
+Console.WriteLine("Видаляємо всі машини з парку:");
+carsPark.RemoveCollection();
+Console.WriteLine("Загальна кількість машин у парку: " + carsPark.Count);
+
