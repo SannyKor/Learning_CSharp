189bc70ded3c6724a7737d77fa39aed74d4be9a0
Olexandr <sanny0787@gmail.com>
2026-04-16 15:28:29 +0300

    feat(Lesson 13): complete task 1

---
+++ CSharp_Essential/Lesson_13_Threads/Task_1/MatrixColumn.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class MatrixColumn
+    {
+        int positionY = 0;
+        int positionX;
+        int bodyLength;
+        int tailPositionY;
+        int screenHeight = Console.WindowHeight;
+
+        private static object lockObject = new object();
+       
+        public void Run()
+        {
+            while (true)
+            {
+                if (Console.WindowHeight != screenHeight)
+                {
+                    lock (lockObject)
+                    {
+                        Console.Clear();
+                    }
+                    screenHeight = Console.WindowHeight;
+                    positionY = 0;
+                }
+                screenHeight = Console.WindowHeight;
+                if (positionY == 0)
+                {
+                    positionX = Random.Shared.Next(0, Console.WindowWidth);
+                    bodyLength = Random.Shared.Next(5, 15);
+                }
+                DrawHead(screenHeight);
+                DrawBody(screenHeight);
+                ClearTail(screenHeight);
+
+                positionY++;
+                if (positionY - bodyLength >= screenHeight)
+                {
+                    positionY = 0;
+                }
+                Thread.Sleep(Random.Shared.Next(0, 50));
+            }
+        }
+        private void DrawHead(int screenHeight)
+        {
+            if (positionY < screenHeight)
+            {
+                lock(lockObject)
+                {
+                    Console.SetCursorPosition(positionX, positionY);
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write((char)Random.Shared.Next(33, 126));
+                }
+            }
+        }
+        private void DrawBody(int screenHeight)
+        {
+            for (int i = 1; i < bodyLength; i++)
+            {
+                int bodyPositionY = positionY - i;
+                if (bodyPositionY >= 0 && bodyPositionY < screenHeight)
+                {
+                    lock (lockObject)
+                    {
+                        Console.SetCursorPosition(positionX, bodyPositionY);
+                        Console.ForegroundColor = i < 3 ? ConsoleColor.Green : ConsoleColor.DarkGreen;
+                        Console.Write((char)Random.Shared.Next(33, 126));
+                    }
+                }
+            }
+        }
+        private void ClearTail(int screenHeight)
+        {
+            tailPositionY = positionY - bodyLength;
+            if (tailPositionY >= 0 && tailPositionY < screenHeight)
+            {
+                lock (lockObject)
+                {
+                    Console.SetCursorPosition(positionX, tailPositionY);
+                    Console.Write(' ');
+                }
+            }
+        }
+    }
+}
+
---
+++ CSharp_Essential/Lesson_13_Threads/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Task_1;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+object lockObj = new object();
+Console.ForegroundColor = ConsoleColor.Green;
+Console.CursorVisible = false;
+
+
+
+for (int i = 0; i < 50; i++)
+{
+    MatrixColumn column = new MatrixColumn();
+    Thread thread = new Thread(column.Run);
+    thread.Start();
+    Thread.Sleep(Random.Shared.Next(50, 500));
+}
+
+
+
+
+
+
