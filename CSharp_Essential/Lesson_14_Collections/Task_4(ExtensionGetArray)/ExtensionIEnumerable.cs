849a9b992786b8e1c51cdef4faaa80c7cfd0c708
Olexandr <sanny0787@gmail.com>
2026-04-19 07:59:57 +0300

    feat(Lesson_14): complete all tasks

---
+++ CSharp_Essential/Lesson_14_Collections/Task_4(ExtensionGetArray)/ExtensionIEnumerable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Task_2;
+
+namespace Task_4_ExtensionGetArray_
+{
+    public static class ExtensionIEnumerable
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            List<T> tempList = new List<T>();
+            foreach (T item in list)
+            {
+                tempList.Add(item);
+            }
+            return tempList.ToArray();
+        }
+    }
+}
