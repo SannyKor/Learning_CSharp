aacb808be44b74d02f0adc0c32e6d5db703ab8d9
Olexandr <sanny0787@gmail.com>
2026-04-24 08:00:31 +0300

    feat(Lesson_16_Operators): implement Task_6(Point3D)

---
+++ CSharp_Essential/Lesson_16_Operators/Task_6(3DPoint)/Point3D.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_6_3DPoint_
+{
+    public struct Point3D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+        public Point3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+        public static Point3D operator +( Point3D p1, Point3D p2)
+        {
+            return new Point3D(p1.X + p2.X, p1.Y + p2.Y, p1.Z + p2.Z);
+        }
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+    }
+}
