bc20e3d0abfdbeba6e000084ba6ec9944cfafe05
Olexandr <sanny0787@gmail.com>
2026-04-26 09:11:00 +0300

    feat(Lesson_17_Linq): complete all tasks

---
+++ CSharp_Essential/Lesson_17_Linq/Task_2(BusStation)/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string PurchasedModel { get; set; }
+    }
+}
---
+++ CSharp_Essential/Lesson_17_Linq/Task_3(MyDictionary)/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+
+var myDictionary = new List<dynamic>
+{
+    new { English = "Apple", Ukrainian = "Яблуко" },
+    new { English = "Table", Ukrainian = "Стіл" },
+    new { English = "Sky", Ukrainian = "Небо" },
+    new { English = "Book", Ukrainian = "Книга" },
+    new { English = "Water", Ukrainian = "Вода" },
+    new { English = "Flower", Ukrainian = "Квітка" },
+    new { English = "Mountain", Ukrainian = "Гора" },
+    new { English = "Friend", Ukrainian = "Друг" },
+    new { English = "Time", Ukrainian = "Час" },
+    new { English = "City", Ukrainian = "Місто" }
+};
+
+foreach (var item in myDictionary)
+{
+    Console.WriteLine($"English: {item.English} = \tUkrainian: {item.Ukrainian}");
+}
