872f4a24dc295665c6a90dd3cf9278222282df1b
Olexandr <sanny0787@gmail.com>
2026-04-30 07:46:09 +0300

    feat(Lesson_18): implement Task_2

---
+++ CSharp_Essential/Lesson_18_Namespaces/Task_2(part_1)/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_1
+{
+    public class Class1
+    {
+        public void ShowHello()
+        {
+            Console.WriteLine("Hello, I am Part_1.Class1!");
+        }
+    }
+}
---
+++ CSharp_Essential/Lesson_18_Namespaces/Task_2(part_2)/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Part_1;
+
+namespace Part_2
+{
+    public class Class1 : Part_1.Class1
+    {
+        public void ShowHelloFromPart2()
+        {
+            Console.WriteLine("Hello, I am Part_2.Class1!");
+        }
+    }
+}
