e8889673d46f3944713353046fc11cdb481cc3ea
Olexandr <sanny0787@gmail.com>
2026-05-15 08:20:22 +0300

    feat(Lesson_1): implement task 1

---
+++ CSharp_Professional/Lesson_1(User_Collections)/Task_1/ClassMonth.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    public enum WeatherSeason
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+    public class ClassMonth
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public int Days { get; set; }
+
+        public WeatherSeason Season { get; set; }
+        public ClassMonth(string name, int number, int days, WeatherSeason season) { 
+            Name = name;
+            Number = number;
+            Days = days;
+            Season = season;
+        }
+        public override string ToString()
+        {
+            return $"Month: {Name}, Number: {Number}, Days: {Days}, Season: {Season}";
+        }
+    }
+}
