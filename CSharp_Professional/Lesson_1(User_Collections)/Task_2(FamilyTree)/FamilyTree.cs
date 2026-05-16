908a219946b372cf1f61772c77fc064ec97c8118
Olexandr <sanny0787@gmail.com>
2026-05-16 09:47:48 +0300

    feat(Lesson 1): WIP - implementing task 2

---
+++ CSharp_Professional/Lesson_1(User_Collections)/Task_2(FamilyTree)/FamilyTree.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Task_2;
+
+namespace Task_2
+{
+    public class FamilyTree : ICollection<Person>
+    {
+        Person? RootMale { get; }
+        Person? RootFemale { get;  }
+        private List<Person> familyMembers = new List<Person>();
+
+        public int Count => familyMembers.Count;
+
+        public bool IsReadOnly => false;
+        public FamilyTree(Person male, Person female)
+        {
+            RootMale = male;
+            RootFemale = female;
+            familyMembers.Add(RootMale);
+            familyMembers.Add(RootFemale);
+        }
+        public void Add(Person descendant)
+        {
+            if (IsDescendant(descendant))
+            { 
+                familyMembers.Add(descendant);
+            }
+            else
+            {
+                Console.WriteLine("This person is not a descendant of the family tree.");
+            }
+        }
+        bool IsDescendant(Person descendant)
+        {
+            if (familyMembers.Contains(descendant.ParentMom) || familyMembers.Contains(descendant.ParentDad))
+            {
+                return true;
+            }
+            else if (IsDescendant(descendant.ParentDad) || IsDescendant(descendant.ParentMom))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            familyMembers.Clear();
+        }
+
+        public bool Contains(Person item)
+        {
+            return familyMembers.Contains(item);
+        }
+
+        public void CopyTo(Person[] array, int arrayIndex)
+        {
+            familyMembers.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<Person> GetEnumerator()
+        {
+            return familyMembers.GetEnumerator();
+        }
+
+        public bool Remove(Person item)
+        {
+            return familyMembers.Remove(item);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return familyMembers.GetEnumerator();
+        }
+    }
+}
---
+++ CSharp_Professional/Lesson_1(User_Collections)/Task_2(FamilyTree)/Person.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_2
+{
+    public enum Gender
+    {
+        Male = 0,
+        Female = 1
+    }
+    public class Person
+    {
+        public DateTime Birthday { get;  }
+        public string? Name { get; }
+        public Gender Gender {
+            get; }
+        List <Person> Children { get; }
+        Person? parentMom = null;
+        public Person? ParentMom { get { return parentMom; } }   
+        Person ? parentDad = null;
+        public Person? ParentDad { get { return parentDad; } }
+        public Person (Person Mom, Person Dad, string? name, DateTime birthday, Gender gender)
+        {
+            this.parentMom = Mom;
+            this.parentDad = Dad;
+            this.Name = name;
+            this.Birthday = birthday;
+            this.Gender = gender;
+            this.Children = new List<Person>();
+        }
+        public Person(Person parent, string? name, DateTime birthday, Gender gender)
+        {
+            if (parent.Gender == Gender.Male)
+            {
+                parentDad = parent;
+            }
+            else
+            {
+                parentMom = parent;
+            }
+            this.Name = name;
+            this.Birthday = birthday;
+            this.Gender = gender;
+            this.Children = new List<Person>();
+        }
+        public Person (string? name, DateTime birthday, Gender gender)
+        {
+            this.Name = name;
+            this.Birthday = birthday;
+            this.Gender = gender;
+            this.Children = new List<Person>();
+        }
+        public Person? this[string name]
+        {
+            get
+            {
+                foreach (var child in Children)
+                {
+                    if (child.Name == name)
+                    {
+                        return child;
+                    }
+                }
+                Console.WriteLine("Child not found.");
+                return null;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Birthday: {Birthday.ToShortDateString()}, Gender: {Gender}";
+        }
+    }
+}
