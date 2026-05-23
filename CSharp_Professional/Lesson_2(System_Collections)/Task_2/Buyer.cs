d90fcea7b6421ebd61627bcff0dd5cc5e247b008
Olexandr <sanny0787@gmail.com>
2026-05-23 10:32:17 +0300

    feat(Lesson_2): WIP implenting task 2

---
+++ CSharp_Professional/Lesson_2(System_Collections)/Task_2/Buyer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Task_2
+{
+    public class Buyer
+    {
+        public string Name { get; set; }  
+        private List<Item> items;
+        public IEnumerable<Item> Items => items;
+        public Buyer(string name)
+        {
+            Name = name;
+            items = new List<Item>();
+        }
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+        }
+        public Item this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= items.Count)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+                return items[index];
+            }
+        }
+    }
+}
---
+++ CSharp_Professional/Lesson_2(System_Collections)/Task_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using Task_2;
+
+
+List<Item> itemsList = new List<Item> { 
+    new Item { Name = "Laptop", Category = "Electronics" },
+    new Item { Name = "Headphones", Category = "Electronics" },
+    new Item { Name = "Screwdriver", Category = "Hand tools" },
+    new Item { Name = "Coffee Maker", Category = "Home Appliances" },
+    new Item { Name = "Blender", Category = "Home Appliances" },
+    new Item { Name = "Smartphone", Category = "Electronics" },
+    new Item { Name = "Hammer", Category = "Hand tools" },
+    new Item { Name = "Vacuum Cleaner", Category = "Home Appliances" },
+    new Item { Name = "Tablet", Category = "Electronics" },
+    new Item { Name = "Air Fryer", Category = "Home Appliances" },
+    new Item { Name = "Wrench", Category = "Hand tools" },
+    new Item { Name = "Smartwatch", Category = "Electronics" },
+    new Item { Name = "Microwave Oven", Category = "Home Appliances" },
+    new Item { Name = "Camera", Category = "Electronics" },
+    new Item { Name = "Drill", Category = "Hand tools" },
+};
+List<Buyer> buyersList = new List<Buyer>{
+    NewBuyer("John Doe", itemsList),
+    NewBuyer("Jane Smith", itemsList),
+    NewBuyer("Alice Johnson", itemsList),
+    NewBuyer("Bob Brown", itemsList),
+    NewBuyer("Charlie Davis", itemsList),
+    NewBuyer("Diana Evans", itemsList),
+    NewBuyer("Frank Green", itemsList)
+};
+
+Dictionary<string, HashSet<string>> buyerToCategories = new Dictionary<string, HashSet<string>>();
+foreach(var buyer in buyersList)
+{
+    if (!buyerToCategories.ContainsKey(buyer.Name))
+    {
+        buyerToCategories[buyer.Name] = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+    }
+    foreach (var item in buyer.Items)
+    {
+        buyerToCategories[buyer.Name].Add(item.Category);
+    }
+}
+static Buyer NewBuyer (string name, List<Item> items)
+{
+    Buyer buyer = new Buyer(name);
+    Random rand = new Random();
+    for (int i = 0; i < rand.Next(1, 20); i++)
+    {
+        int randomIndex = rand.Next(items.Count);
+        buyer.AddItem(items[randomIndex]);
+    }
+    return buyer;
+}
