9847e8e3ab915d303f3092c851aef369eb502cbc
Olexandr <sanny0787@gmail.com>
2026-05-29 07:34:20 +0300

    feat(Lesson_2): WIP implementin task 4 and 6

---
+++ CSharp_Professional/Lesson_2(System_Collections)/Task_4(OrderedDictionry)/OrderedDictionary.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_4_OrderedDictionry_
+{
+    public class MyOrderedDictionary<TKey, TValue> : ICollection<KeyValuePair<TKey, TValue>> where TKey : notnull
+    {
+        private Dictionary<TKey, TValue> _dictionary = new Dictionary<TKey, TValue>();
+        private List <TKey> _order = new List<TKey>();
+
+        public int Count => _dictionary.Count;
+
+        public bool IsReadOnly => false;
+        public TValue this[TKey key]
+        {
+            get => _dictionary[key];
+            set
+            {
+                if(!_dictionary.ContainsKey(key))
+                {
+                    _order.Add(key);
+                }
+                _dictionary[key] = value;
+            }
+        }
+        public TValue GetAtIndex(int index)
+        {
+            if ((index >= _order.Count)||index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            return _dictionary[_order[index]];
+        }
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            Add(item.Key, item.Value);
+        }
+        public void Add(TKey key, TValue value)
+        {
+            _dictionary.Add(key, value);
+            _order.Add(key);
+        }
+
+        public void Clear()
+        {
+            _dictionary.Clear();
+            _order.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return _dictionary.TryGetValue(item.Key, out TValue value) && EqualityComparer<TValue>.Default.Equals(value, item.Value);
+        }
+        public bool ContainsKey(TKey key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+        public bool ContainsValue(TValue value)
+        {
+            return _dictionary.ContainsValue(value);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            foreach(var key in _order)
+            {
+                yield return new KeyValuePair<TKey, TValue>(key, _dictionary[key]);
+            }
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+        public bool Remove(TKey key)
+        {
+            if(key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            bool removed = _dictionary.Remove(key);
+            if(removed)
+            {
+                _order.Remove(key);
+            }
+            return removed;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CSharp_Professional/Lesson_2(System_Collections)/Task_6(SortedList)/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+SortedList<string, decimal> fruits = new SortedList<string, decimal>
+{
+    { "Яблуко", 1.20m },
+    { "Банан", 0.80m },
+    { "Вишня", 2.50m },
+    { "Груша", 1.50m },
+    { "Апельсин", 1.00m },
+    { "Ківі", 1.80m },
+    { "Манго", 2.00m },
+    { "Ананас", 3.00m },
+    { "Лимон", 0.90m },
+    { "Гранат", 2.20m },
+    { "Персик", 1.70m },
+    { "Слива", 1.30m },
+    { "Малина", 2.80m },
+    { "Лохина", 3.50m },
+    { "Кавун", 0.60m }
+};
+Console.WriteLine("Список фруктів та їх ціни в алфавітному порядку:");
+foreach (var fruit in fruits)
+{
+    Console.WriteLine($"\t{fruit.Key} \t- \t{fruit.Value} грн;");
+}
+Console.WriteLine(new string('-', 40));
+
+Console.WriteLine("Список фруктів та їх ціни в зворотному алфавітному порядку:");
+foreach (var fruit in fruits.Reverse())
+{
+    Console.WriteLine($"\t{fruit.Key} \t- \t{fruit.Value} грн;");
+}
