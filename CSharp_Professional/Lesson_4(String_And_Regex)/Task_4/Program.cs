c5fb33581de3314ae5ff793d39a83bbe96c533ac
Olexandr <sanny0787@gmail.com>
2026-06-15 09:41:28 +0300

    feat(Lesson_4): WIP - task 4

--- CSharp_Professional/Lesson_4(String_And_Regex)/Task_4/Program.cs
+++ CSharp_Professional/Lesson_4(String_And_Regex)/Task_4/Program.cs
@@ -8,3 +8,18 @@ var usaCulture = new CultureInfo("en-US");
 
 using (StreamReader reader = new StreamReader(fileName))
 {
+    string? dateLine = reader.ReadLine();
+    var date = DateTime.Parse(dateLine);
+
+    List<(string name, decimal price)> items = new();
+    string? line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        string[] parts = line.Split(";");
+        if (parts.Length == 2)
+        {
+            items.Add((parts[0], decimal.Parse(parts[1], currentCulture)));
+        }
+    }
+}
+
