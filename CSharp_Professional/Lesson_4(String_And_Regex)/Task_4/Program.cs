5824c49d3216fc3f29f520180e8ea685a8a4ad37
Olexandr <sanny0787@gmail.com>
2026-06-16 00:26:28 +0300

    feat(Lesson 4): complete all tasks

--- CSharp_Professional/Lesson_4(String_And_Regex)/Task_4/Program.cs
+++ CSharp_Professional/Lesson_4(String_And_Regex)/Task_4/Program.cs
@@ -2,24 +2,42 @@
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-string fileName = "receipt.txt";
-var currentCulture = CultureInfo.CurrentCulture;
-var usaCulture = new CultureInfo("en-US");
+string path = "receipt.txt";
 
-using (StreamReader reader = new StreamReader(fileName))
+
+using (StreamReader reader = new StreamReader(path))
 {
     string? dateLine = reader.ReadLine();
     var date = DateTime.Parse(dateLine);
 
-    List<(string name, decimal price)> items = new();
+    //ліст закриваємо кортежем щоб розділити назву та ціну
+    List<(string Name, decimal Price)> items = new();
     string? line;
     while ((line = reader.ReadLine()) != null)
     {
-        string[] parts = line.Split(";");
+        string[] parts = line.Split(":");
         if (parts.Length == 2)
         {
-            items.Add((parts[0], decimal.Parse(parts[1], currentCulture)));
+            items.Add((parts[0], decimal.Parse(parts[1], CultureInfo.InvariantCulture)));
         }
     }
+    Console.WriteLine("=== Поточна локаль ===");
+    PrintReceipt(date, items, CultureInfo.CurrentCulture);
+
+    Console.WriteLine();
+
+    Console.WriteLine("=== Локаль en-US ===");
+    PrintReceipt(date, items, new CultureInfo("en-US"));
+}
+static void PrintReceipt(DateTime date, List<(string Name, decimal Price)> items, CultureInfo culture)
+{
+    decimal total = 0;
+    Console.WriteLine($"Date: {date.ToString(culture)}");
+    foreach(var item in items)
+    {
+        Console.WriteLine($"{item.Name}: {item.Price.ToString("C", culture)}");
+        total += item.Price;
+    }
+    Console.WriteLine($"Total: {total.ToString("C", culture)}"); 
 }
 
