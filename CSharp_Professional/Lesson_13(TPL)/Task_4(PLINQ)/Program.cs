71114fb0396f00e951092bb9d10c703247776e52
Olexandr <sanny0787@gmail.com>
2026-07-22 07:46:12 +0300

    feat(Lesson_13(TPL)): implement task 4 using Parallel LINQ

---
+++ CSharp_Professional/Lesson_13(TPL)/Task_4(PLINQ)/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_4_PLINQ_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int[] numbers = new int[1000000];
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 1000000);
+            }
+
+            var oddNumbers = numbers
+                .AsParallel()
+                .Where(number => number % 2 != 0)
+                .ToArray();
+
+            Console.WriteLine($"Знайдено {oddNumbers.Length} непарних чисел.");
+            Console.WriteLine("Перші 10 непарних чисел:");
+            foreach (var number in oddNumbers.Take(10))
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
